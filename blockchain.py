c4278de0393876fbfa54e1a089b5ca8c440076fe
Alex Rojas <alex.olmedo.rojas@gmail.com>
2022-03-23 21:46:48 -0600

    Add dunder method to represent the chain with a string

--- blockchain.py
+++ blockchain.py
@@ -19,6 +19,9 @@ class Blockchain:
     def add_block(self, data):
         self.chain.append(Block(data))
 
+    def __repr__(self):
+        print(f'Blockchain: {self.chain}')
+
 blockchain = Blockchain()
 blockchain.add_block('one')
 blockchain.add_block('two')
