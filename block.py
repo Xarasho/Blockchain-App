6d141f7d63bed1c672893566622dace50194cce3
Alex Rojas <alex.olmedo.rojas@gmail.com>
2022-03-24 11:56:37 -0600

    Add timestamp, last_hash and hash parameters to Block class

--- block.py
+++ block.py
@@ -4,11 +4,20 @@ class Block:
     Store transactions in a blockchain that supports a cryptocurrency.
     """
 
-    def __init__(self, data):
+    def __init__(self, timestamp, last_hash, hash, data):
+        self.timestamp = timestamp
+        self.last_hash = last_hash
+        self.hash = hash
         self.data = data
 
     def __repr__(self):
-        return f'Block - data: {self.data}'
+        return (
+          'Block('
+          f'timestamp: {self.timestamp}, '
+          f'last_hash: {self.last_hash}, '
+          f'hash: {self.hash}, '
+          f'data: {self.data})'
+        )
 
 def main():
     block = Block('foo')
