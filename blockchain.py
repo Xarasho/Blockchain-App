fbd469ec60b8bfbb7cde78f339ce9d80e437b933
Alex Rojas <alex.olmedo.rojas@gmail.com>
2022-03-23 21:25:45 -0600

    Add block class

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,14 @@
+class Block:
+    """
+    Block: a unit of storage.
+    Store transactions in a blockchain that supports a cryptocurrency.
+    """
+
 class Blockchain:
-  """
-  Blockchain: a public ledger of transactions.
-  Implemented as a set of blocks - data sets of transactions
-  """
+    """
+    Blockchain: a public ledger of transactions.
+    Implemented as a set of blocks - data sets of transactions.
+    """
+
+    def __init__(self):
+        self.chain = []
